7089a2a683f85063cb1465b809ed91d758723274
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2021-05-19 13:02:54 +0100

    added binary search tree

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -37,6 +37,115 @@ class BST {
             }
         }
     }
+
+     
+    remove(value){
+        this.root = this.removeNode(this.root, value)
+    }
+    // a recursive function to insert a new value in binary search tree
+    
+    removeNode(current, value) {
+        
+        // base case, if the tree is empty 
+        
+       if(current === null) return current
+        
+        // when value is the same as current's value, this is the node to be deleted
+        
+        if (value === current.value) {
+             
+            // for case 1 and 2, node without child or with one child
+            
+            if (current.left === null && current.right === null){
+                
+                    return null
+                
+                }else if(current.left === null){
+                
+                    return current.right 
+             
+                }else if(current.right === null){
+                
+                    return current.left
+                
+                }else{
+                    
+                    /// node with two children, get the inorder successor, 
+                    //smallest in the right subtree
+                    
+                    let tempNode = this.kthSmallestNode(current.right)
+                        current.value = tempNode.value
+                    
+                    /// delete the inorder successor
+                    
+                        current.right = this.removeNode(current.right, tempNode.value)
+                    return current
+            }
+
+        // recur down the tree
+            
+        }else if(value < current.value) {
+            
+            current.left = this.removeNode(current.left, value)
+            return current
+            
+        }else{
+            
+            current.right = this.removeNode(current.right, value)
+            return current
+        }
+    }
+    
+     /// helper function to find the smallest node
+    
+    kthSmallestNode(node) {
+        while(!node.left === null)
+            node = node.left
+
+        return node
+    }
+
+
+    
+}
+
+// traversal functions
+
+function inOrderTraversal(root){
+    if(root === null){
+        return []
+    }
+    const result=[]
+    if(root.left !== null){result.push(...inOrderTraversal(root.left))}
+    result.push(root.data)
+    if(root.right !== null){ result.push(...inOrderTraversal(root.right))}
+    return result
+}
+
+
+
+function preOrderTraversal(root){
+    if(root === null){
+        return []
+    }
+    const result=[]
+    result.push(root.data)
+    if(root.left !== null){result.push(...preOrderTraversal(root.left))}
+    if(root.right !== null){ result.push(...preOrderTraversal(root.right))}
+    return result
+}
+
+
+function postOrderTraversal(root){
+    if(root === null){
+        return []
+    }
+    const result=[]
+    
+    if(root.left !== null){result.push(...postOrderTraversal(root.left))}
+    if(root.right !== null){ result.push(...postOrderTraversal(root.right))}
+    result.push(root.data)
+    return result
 }
 
 
@@ -52,5 +161,9 @@ bst.insert(9)
 bst.insert(54)
 bst.insert(4)
 bst.insert(2)
+console.log()
+// console.log(JSON.stringify(bst))
 
-console.log(JSON.stringify(bst))
+console.log(inOrderTraversal(bst.root))
+console.log(preOrderTraversal(bst.root))
+console.log(postOrderTraversal(bst.root))
