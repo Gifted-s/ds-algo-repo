c5e11369f26e3242c7bd11bbd0f7422bed5b666e
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-05-03 13:35:48 +0100

    added three sum

---
+++ three-sum/three-sum.js
@@ -0,0 +1,64 @@
+// Copyright Adewumi Sunkanmi 2022
+// 3Sum
+
+// 17543
+
+// 1680
+
+// Add to List
+
+// Share
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Example 2:
+
+// Input: nums = []
+// Output: []
+// Example 3:
+
+// Input: nums = [0]
+// Output: []
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+    const result = []
+    nums=nums.sort((a,b)=> a-b)
+    for (let i=0; i<nums.length; i++){
+        let current = nums[i]
+        if(i !== 0 && nums[i-1] === current){
+            continue
+        }
+        let left = i+1
+        let right= nums.length-1;
+        while(left < right){
+            const threeSum = current + nums[left] + nums[right]
+            if(threeSum >0){
+                right--
+            }
+            else if(threeSum <0){
+                left++
+            }
+            else{
+            result.push([current, nums[left], nums[right]])
+            left++
+                while(nums[left] === nums[left-1] && left< right){
+                    left ++
+                }
+            }
+           
+        }
+    }
+     return result
+ };
