5e4f18069fb711b5ffdf32cac540938b49446d01
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-05-11 00:14:35 +0100

    added generate parentheses algorithm

---
+++ Generate_Parentheses/generate_parentheses.js
@@ -0,0 +1,55 @@
+// Copyright Adewumi Sunkanmi 2022
+// 22. Generate Parentheses
+// Medium
+
+// 12761
+
+// 491
+
+// Add to List
+
+// Share
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+ 
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var generateParenthesis = function(n) {
+    // only add open if open is < n
+    // only add close bracket if closeN < open
+    // valid if closeN is === openN 
+    
+    let result =[]
+    let stack =[]
+    function backTrack (openN, closeN){
+        if(openN===n && closeN===n){
+            result.push(stack.join(""))
+            return
+        }
+        if(openN< n){
+            stack.push("(")
+            backTrack(openN+1, closeN)
+            stack.pop()
+        }
+        
+        if(closeN< openN){
+            stack.push(")")
+            backTrack(openN, closeN+1)
+            stack.pop()
+        } 
+    }
+    backTrack(0,0)
+    return result
+};
