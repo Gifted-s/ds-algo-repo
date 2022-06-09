a2f2235961e5e825d89c054438aff7ded5e7475e
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-09 17:19:49 +0100

    diameter of binary tree

---
+++ Diameter-of-Binary-Tree/diameter-of-binary-tree.js
@@ -0,0 +1,63 @@
+// Copyright Adewumi Sunkanmi 2022  
+// 543. Diameter of Binary Tree
+// Easy
+
+// 8079
+
+// 501
+
+// Add to List
+
+// Share
+// Given the root of a binary tree, return the length of the diameter of the tree.
+
+// The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+// The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5]
+// Output: 3
+// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+// Example 2:
+
+// Input: root = [1,2]
+// Output: 1
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// -100 <= Node.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var diameterOfBinaryTree = function(root) {
+    let res= [0];
+    function dfs(root){
+        if(!root){
+            return -1
+        }
+        let left = dfs(root.left)
+        let right = dfs(root.right)
+        res[0] = Math.max(res[0], (2 + left+right))
+        return 1 + Math.max(left, right)
+    }
+    dfs(root)
+    return res[0];
+};
