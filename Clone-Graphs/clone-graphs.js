3fd73faad02c2fe866cd5137f132870d17378eb3
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-06-28 22:34:07 +0100

    added clone-graphs algorithm

---
+++ Clone-Graphs/clone-graphs.js
@@ -0,0 +1,78 @@
+/**
+// Adewumi Sunkanmi 2022
+133. Clone Graph
+Medium
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+ 
+
+Example 1:
+
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+Example 2:
+
+
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+
+
+* // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+ var cloneGraph = function(node) {
+    if(node === null){
+        return null
+    }
+    
+    let copyMap = new Map() // map each node to its copy
+    
+    function dfs(node){
+        if(copyMap.has(node)){
+            return copyMap.get(node)
+        }
+        
+        let copy = new Node(node.val)
+        copyMap.set(node, copy)
+        for(let nei of node.neighbors){
+            copy.neighbors.push(dfs(nei))
+        }
+        return copy
+    }
+    
+    return dfs(node)
+    
+};
