79d9f155796f582c84c8ab615077c1a0ab3758d4
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-07-18 21:34:26 +0100

    added max sub array.go, work no dey finish

---
+++ Max-Subarray/max-subarray.go
@@ -0,0 +1,47 @@
+// Adewumi Sunkanmi 2022
+package maxsubarray
+
+import "math"
+// 53. Maximum Subarray
+// Medium
+
+// 22851
+
+// 1113
+
+// Add to List
+
+// Share
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+
+func maxSubArray(nums []int) int {
+    maxSub := nums[0]
+    curSum := 0
+    
+    for i:=0;i< len(nums);i++{
+        if curSum < 0 {
+            curSum = 0
+        }
+        curSum = curSum + nums[i]
+        maxSub = int(math.Max(float64(curSum),float64(maxSub)))
+    }
+    return maxSub
+}
