224a7ed2886e16837ea9db1705f91aaecaefe9bd
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-07-18 13:36:19 +0100

    best time to buy and sell stocks

---
+++ Best-TIme-To-Buy-And-Sell-Stocks/best-time-to-buy-and-sell-stocks.js
@@ -0,0 +1,65 @@
+// Adewumi Sunkanmi 2022
+// 309. Best Time to Buy and Sell Stock with Cooldown
+// Medium
+
+// 6159
+
+// 216
+
+// Add to List
+
+// Share
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+// After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+// Example 1:
+
+// Input: prices = [1,2,3,0,2]
+// Output: 3
+// Explanation: transactions = [buy, sell, cooldown, buy, sell]
+// Example 2:
+
+// Input: prices = [1]
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= prices.length <= 5000
+// 0 <= prices[i] <= 1000
+
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let dp = {}
+    
+    function dfs(i, buying){
+        if(i >= prices.length){
+            return 0
+        }
+        if([i, buying].toString() in dp ){
+            return dp[[i, buying].toString()] 
+        }
+          let cooldownProfit= dfs(i+1, buying)
+        if(buying){
+            let buyProfit = dfs(i+1, !buying) - prices[i]
+            dp [[i, buying].toString()] = Math.max(buyProfit,cooldownProfit)
+        }
+        else{
+             let sellProfit = dfs(i+2, !buying) + prices[i]
+             dp [[i, buying].toString()] = Math.max(sellProfit,cooldownProfit)
+        }
+        return dp[[i, buying].toString()]
+    }
+    
+    return dfs(0, true)
+};
