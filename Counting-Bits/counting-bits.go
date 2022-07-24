4d1f21ad249a6d6320a63f0dedb2bba344a88bef
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-07-24 21:46:35 +0100

    counting bits

---
+++ Counting-Bits/counting-bits.go
@@ -0,0 +1,49 @@
+package countingbits
+// Adewumi Sunkanmi 2022
+// 338. Counting Bits
+// Easy
+
+// 7388
+
+// 346
+
+// Add to List
+
+// Share
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// Example 2:
+
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+
+
+func countBits(n int) []int {
+    dp := []int{0}
+    offset:=1;
+    for i:=1; i<n+1;i++{
+        if offset*2==i{
+            offset = i
+        }
+        dp = append(dp, 1 + dp[i-offset])
+    }
+    
+    return dp
+}
