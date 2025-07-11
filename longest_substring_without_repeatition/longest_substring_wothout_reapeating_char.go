2f94581f055c7b365a5c7c82c4c83715ec81a22a
Adewumi Sunkanmi <sunkanmiadewumi1@gmail.com>
2025-07-11 22:46:05 +0100

    longest substring without repeating characters

--- longest_substring_without_repeatition/longest_substring_wothout_reapeating_char.go
+++ longest_substring_without_repeatition/longest_substring_wothout_reapeating_char.go
@@ -1,4 +1,26 @@
 package longestsubstringwithoutrepeatition
+func lengthOfLongestSubstringV2(s string) int {
+    if len(s) == 0{
+        return 0
+    }
+    hash:= make(map[byte]bool)
+    l:=0
+    max:=0
+    for r:=0; r<len(s); r++ {
+        if _, ok:=hash[s[r]]; ok{
+         for hash[s[r]] {
+            delete(hash, s[l])
+            l++
+         }
+        }
+        dif := r-l+1
+        if dif > max{
+            max = dif
+         }
+        hash[s[r]]=true
+    }
+    return max
+}
 
 func lengthOfLongestSubstring(s string) int {
 	l := 0
